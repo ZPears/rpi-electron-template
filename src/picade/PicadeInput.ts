0fc317a7f846ba95e6f142dd239ec6393838dace
zpears <zachary.p.pearson@gmail.com>
2020-12-18 15:03:23 -0500

    added picade input names

--- src/picade/PicadeInput.ts
+++ src/picade/PicadeInput.ts
@@ -19,32 +19,41 @@ export enum PicadeInput {
 
 interface Mapping {
   input: PicadeInput,
-  keyCode: number
+  keyCode: number,
+  keyName: string
 }
 
 const mappings: Array<Mapping> = [
-  { input: PicadeInput.JOYSTICK_UP, keyCode: 38 },
-  { input: PicadeInput.JOYSTICK_LEFT, keyCode: 37 },
-  { input: PicadeInput.JOYSTICK_DOWN, keyCode: 40 },
-  { input: PicadeInput.JOYSTICK_RIGHT, keyCode: 39 },
-  { input: PicadeInput.BUTTON_A, keyCode: 90 },
-  { input: PicadeInput.BUTTON_B, keyCode: 16 },
-  { input: PicadeInput.BUTTON_X, keyCode: 18 },
-  { input: PicadeInput.BUTTON_Y, keyCode: 17 },
-  { input: PicadeInput.BUTTON_LB, keyCode: 32 },
-  { input: PicadeInput.BUTTON_RB, keyCode: 88 },
-  { input: PicadeInput.BUTTON_LT, keyCode: 27 },
-  { input: PicadeInput.BUTTON_RT, keyCode: 13 },
-  { input: PicadeInput.START, keyCode: 79 },
-  { input: PicadeInput.SELECT, keyCode: 73 }
+  { input: PicadeInput.JOYSTICK_UP, keyCode: 38, keyName: "ArrowUp" },
+  { input: PicadeInput.JOYSTICK_LEFT, keyCode: 37, keyName: "ArrowLeft" },
+  { input: PicadeInput.JOYSTICK_DOWN, keyCode: 40, keyName: "ArrowDown" },
+  { input: PicadeInput.JOYSTICK_RIGHT, keyCode: 39, keyName: "ArrowRight" },
+  { input: PicadeInput.BUTTON_A, keyCode: 90, keyName: "z" },
+  { input: PicadeInput.BUTTON_B, keyCode: 16, keyName: "Shift" },
+  { input: PicadeInput.BUTTON_X, keyCode: 18, keyName: "Alt" },
+  { input: PicadeInput.BUTTON_Y, keyCode: 17, keyName: "Control" },
+  { input: PicadeInput.BUTTON_LB, keyCode: 32, keyName: " " },
+  { input: PicadeInput.BUTTON_RB, keyCode: 88, keyName: "x" },
+  { input: PicadeInput.BUTTON_LT, keyCode: 27, keyName: "Escape" },
+  { input: PicadeInput.BUTTON_RT, keyCode: 13, keyName: "Enter" },
+  { input: PicadeInput.START, keyCode: 79, keyName: "o" },
+  { input: PicadeInput.SELECT, keyCode: 73, keyName: "i" }
 ]
 
 const picadeToKeyCode: Map<PicadeInput, number> = new Map(
   mappings.map(mapping => [mapping.input, mapping.keyCode])
 )
 
-const keyCodeForPicadeInput = (picadeInput: PicadeInput) => {
-  return picadeToKeyCode.get(picadeInput);
+const picadeToKeyName: Map<PicadeInput, string> = new Map(
+  mappings.map(mapping => [mapping.input, mapping.keyName])
+)
+
+const keyCodeForPicadeInput = (picadeInput: PicadeInput): number => {
+  return picadeToKeyCode.get(picadeInput)!;
+}
+
+export const keyNameForPicadeInput = (picadeInput: PicadeInput): string => {
+  return picadeToKeyName.get(picadeInput)!;
 }
 
 export default keyCodeForPicadeInput;
